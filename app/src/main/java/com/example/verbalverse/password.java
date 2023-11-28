f889f72857e059a6165b4ebe9cd42e893e2ed953
tuilavannhi <52100984@student.tdtu.edu.vn>
2023-11-28 15:14:20 +0700

    init project

---
+++ app/src/main/java/com/example/verbalverse/password.java
@@ -0,0 +1,13 @@
+package com.example.verbalverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+    }
+}
