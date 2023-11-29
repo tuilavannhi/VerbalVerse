c50935a9ec31483d6d8726900effb2fefdb1090c
tuilavannhi <52100984@student.tdtu.edu.vn>
2023-11-29 20:32:20 +0700

    init project

---
+++ app/src/main/java/com/example/verbalverse/AddHocPhan.java
@@ -0,0 +1,14 @@
+package com.example.verbalverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddHocPhan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_hoc_phan);
+    }
+}
